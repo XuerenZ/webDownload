697f9cbcff21d19c03f5096eba552d2b2d74bfa5
xuerenZ <328272127@qq.com>
2016-08-20 19:21:52 +0800

    创建新工程完成

---
+++ downLoadPicture01/downLoadPicture01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  downLoadPicture01
+//
+//  Created by mac on 16/8/20.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ downLoadPicture01/downLoadPicture01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  downLoadPicture01
+//
+//  Created by mac on 16/8/20.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
